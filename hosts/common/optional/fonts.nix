af408a6518a467ae069dd81a76b49a97293bade2
antoniosarro <tech@antoniosarro.dev>
2025-11-07 20:38:25 +0100

    fix

--- hosts/common/optional/fonts.nix
+++ hosts/common/optional/fonts.nix
@@ -4,38 +4,51 @@
 }:
 {
   fonts = {
+    # WARNING: Disabling enableDefaultPackages will mess up fonts on sites like
+    # https://without.boats/blog/pinned-places/ with huge gaps after the ' character
     enableDefaultPackages = true;
     fontDir.enable = true;
     packages = (
       builtins.attrValues {
         inherit (pkgs)
-          eb-garamond
-          overpass
+          # icon fonts
+          material-design-icons
+          font-awesome
+
           noto-fonts
           noto-fonts-emoji
-          ;
+          # noto-fonts-extra
 
+          source-sans
+          source-serif
+          ;
         inherit (pkgs.unstable.nerd-fonts)
+          fira-mono
           iosevka
+          jetbrains-mono
           symbols-only
           ;
       }
     );
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would sometimes show instead of some Color emojis
     fontconfig.defaultFonts = {
       serif = [
         "Noto Color Emoji"
-        "EB Garamond"
+        "Iosevka Nerd Font Mono"
         "Nerd Fonts Symbols Only"
       ];
       sansSerif = [
         "Noto Color Emoji"
         "Nerd Fonts Symbols Only"
-        "Overpass"
+        "FiraMono Nerd Font Mono"
       ];
       monospace = [
+        "JetBrainsMono Nerd Font"
         "Noto Color Emoji"
         "Iosevka Nerd Font Mono"
         "Nerd Fonts Symbols Only"
+        "FiraMono Nerd Font Mono"
       ];
       emoji = [
         "Noto Color Emoji"
